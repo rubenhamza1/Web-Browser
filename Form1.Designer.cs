56f647460b75512202d7fd4beae927c1d5c5921a
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-05-31 11:52:12 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WebPreglednik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtBrowser = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsHome = new System.Windows.Forms.ToolStripButton();
+            this.tsBack = new System.Windows.Forms.ToolStripButton();
+            this.tsForward = new System.Windows.Forms.ToolStripButton();
+            this.tsNewtab = new System.Windows.Forms.ToolStripButton();
+            this.tsRefresh = new System.Windows.Forms.ToolStripButton();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBrowser
+            // 
+            this.txtBrowser.Location = new System.Drawing.Point(13, 38);
+            this.txtBrowser.Name = "txtBrowser";
+            this.txtBrowser.Size = new System.Drawing.Size(966, 20);
+            this.txtBrowser.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(13, 71);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1052, 497);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsHome,
+            this.tsBack,
+            this.tsForward,
+            this.tsNewtab,
+            this.tsRefresh});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1081, 25);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsHome
+            // 
+            this.tsHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsHome.Image = global::WebPreglednik.Properties.Resources.fsfsfds;
+            this.tsHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsHome.Name = "tsHome";
+            this.tsHome.Size = new System.Drawing.Size(23, 22);
+            this.tsHome.Text = "Home";
+            this.tsHome.Click += new System.EventHandler(this.tsHome_Click);
+            // 
+            // tsBack
+            // 
+            this.tsBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsBack.Image = global::WebPreglednik.Properties.Resources.arrowleft;
+            this.tsBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsBack.Name = "tsBack";
+            this.tsBack.Size = new System.Drawing.Size(23, 22);
+            this.tsBack.Text = "Back";
+            this.tsBack.Click += new System.EventHandler(this.tsBack_Click);
+            // 
+            // tsForward
+            // 
+            this.tsForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsForward.Image = global::WebPreglednik.Properties.Resources.arrowright1;
+            this.tsForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsForward.Name = "tsForward";
+            this.tsForward.Size = new System.Drawing.Size(23, 22);
+            this.tsForward.Text = "Forward";
+            this.tsForward.Click += new System.EventHandler(this.tsForward_Click);
+            // 
+            // tsNewtab
+            // 
+            this.tsNewtab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsNewtab.Image = global::WebPreglednik.Properties.Resources.newtab;
+            this.tsNewtab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsNewtab.Name = "tsNewtab";
+            this.tsNewtab.Size = new System.Drawing.Size(23, 22);
+            this.tsNewtab.Text = "New Tab";
+            this.tsNewtab.Click += new System.EventHandler(this.tsNewtab_Click);
+            // 
+            // tsRefresh
+            // 
+            this.tsRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsRefresh.Image = global::WebPreglednik.Properties.Resources.refresh;
+            this.tsRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsRefresh.Name = "tsRefresh";
+            this.tsRefresh.Size = new System.Drawing.Size(23, 22);
+            this.tsRefresh.Text = "Refresh";
+            this.tsRefresh.Click += new System.EventHandler(this.tsRefresh_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnGo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGo.BackgroundImage")));
+            this.btnGo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnGo.ForeColor = System.Drawing.Color.Black;
+            this.btnGo.Image = ((System.Drawing.Image)(resources.GetObject("btnGo.Image")));
+            this.btnGo.Location = new System.Drawing.Point(996, 28);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(36, 37);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.UseVisualStyleBackColor = false;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1081, 580);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtBrowser);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtBrowser;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsHome;
+        private System.Windows.Forms.ToolStripButton tsBack;
+        private System.Windows.Forms.ToolStripButton tsForward;
+        private System.Windows.Forms.ToolStripButton tsNewtab;
+        private System.Windows.Forms.ToolStripButton tsRefresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WebPreglednik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+      
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            tabControl1.Navigate(txtBrowser.Text);
+        }
+
+        private void tsHome_Click(object sender, EventArgs e)
+        {
+            webbrowser1.GoHome();
+        }
+
+        private void tsBack_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void tsForward_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void tsNewtab_Click(object sender, EventArgs e)
+        {
+            TabPage tabpage = new TabPage();
+            tabpage.Text = "New Tab";
+            tabControl1.Controls.Add(tabpage);
+            WebBrowser webbrowser = new WebBrowser();
+            webbrowser.Parent = tabpage;
+            webbrowser.Dock = DockStyle.Fill;
+            webbrowser.Navigate("www.google.com");
+        }
+
+        private void tsRefresh_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Refresh();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
